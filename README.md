7890e179f43b419bc30460bc441eefb3d47eec83
oliverthill <oliverthill05@gmail.com>
2025-05-04 19:54:14 -0400

    Final submission for web102 prework

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-# WEB102 Prework - *Name of App Here*
+# WEB102 Prework - *Sea Monster Website*
 
-Submitted by: **Your Name Here**
+Submitted by: **Oliver Toshiki Hill**
 
-**Name of your app** is a website for the company Sea Monster Crowdfunding that displays information about the games they have funded.
+**Sea Monster Website** is a website for the company Sea Monster Crowdfunding that displays information about the games they have funded.
 
-Time spent: **X** hours spent in total
+Time spent: **10** hours spent in total
 
 ## Required Features
 
@@ -17,13 +17,14 @@ The following **required** functionality is completed:
 
 The following **optional** features are implemented:
 
-* [ ] List anything else that you can get done to improve the app functionality!
+* [ ] The header includes a button that scrolls to the "Our Games" section.
+* [ ] The website's visual appeal has been improved through custom edits in the html and CSS files.
 
 ## Video Walkthrough
 
 Here's a walkthrough of implemented features:
 
-<img src='http://i.imgur.com/link/to/your/gif/file.gif' title='Video Walkthrough' width='' alt='Video Walkthrough' />
+<img src='https://imgur.com/gallery/web102-walkthrough-VlKxRGf' title='web102_walkthrough' width='' alt='web102_walkthrough' />
 
 <!-- Replace this with whatever GIF tool you used! -->
 GIF created with ...  
@@ -34,11 +35,48 @@ GIF created with ...
 
 ## Notes
 
-Describe any challenges encountered while building the app.
+Describe any challenges encountered while building the app:
+
+
+Problem 1: Lack of familiarity with html and CSS functionality specifically in JavaScript.
+
+I once consulted with a student who was struggling academically to capture requirements for a personalized tracking tool. Based on those needs, I used Java with Selenium to automate interaction with the school portal by inspecting the portal’s DOM in Chrome DevTools. I identified anchor names and file paths, then leveraged CSS and XPath selectors to programmatically extract assignment grades and due dates. While this provided great experience necessary for completing this project, I lack experience with using Javascript specifically. While concepts were familiar, specifics of syntax were not, and methods such as filter() and reduce() were new to me.
+
+Solution: Extensive research through the materials provided and independent resouces.
+
+
+Problem 2: Finding the variable to be passed to addGamesToPage.
+
+I was originally unable to differentiate between GAMES_JSON and gamesContainer, misunderstanding the key difference in their data structures and how they were formed. I thought that gamesContainer itself was a JavaScript array of objects, and did not properly think about the statement:
+
+    const gamesContainer = document.getElementById("games-container");
+
+When reading over this statement again, it can of course be seen that this searches for an element in the html file, meaning that it is an html element and not an array. I overlooked this because it was already provided in the skeleton, but I should have thoroughly scanned all of the given code before moving onto my own implementation.
+
+Solution: Walking through the code step-by-step, and understanding what every object and variable's function is within the code.
+
+
+Problem 3: Various syntax errors, leading to errors in button functionality.
+
+While there were many small cases of syntax errors that I quickly caught, the most notable example was in challenge 6, where my string that explained the number of unfunded games using the ternary operator was not being displayed correctly. I unfortunately spent over 45 minutes trying to debug this one issue only to find that this declaration:
+
+    numOfUnfunded = listOfUnfundedGames.length;
+
+had failed to have been assigned a data type.
+
+Solution: I eventually began to research general debugging methods that could be used for WebDev, and came across the console.log() method. I then inserted in my code: console.log(numOfUnfunded); right after the parsing of the string, and looked at the result in the console tab in Chrome DevTools. I found the error: "Uncaught ReferenceError: numOfUnfunded is not defined" which eventally led me to realize that the underlying issue was the numOfUnfunded variable itself.
+
+
+Problem 4: The scroll button, and making other general CSS edits
+
+In the selenium webscraper program I developed for my client, I was much less concerned with CSS formatting which led me to have less experience with it. Additionally, I had a lot of trouble developing the method for scrolling to a desired section on the web page when assigned to an event listener for a button.
+
+Solution: Extensive research through the materials provided and independent resouces.
+
 
 ## License
 
-    Copyright [yyyy] [name of copyright owner]
+    Copyright [2025] [Oliver Toshiki Hill]
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
--- index.js
+++ index.js
@@ -28,28 +28,35 @@ const gamesContainer = document.getElementById("games-container");
 // create a function that adds all data from the games array to the page
 function addGamesToPage(games) {
 
-    // loop over each item in the data
 
-
-        // create a new div element, which will become the game card
-
+    for (let i = 0; i < games.length; i++) {
+        const game = games[i];
 
         // add the class game-card to the list
+        const gameCard = document.createElement("div");
 
+        // add the class game-card to the list
+        gameCard.classList.add("game-card");
 
         // set the inner HTML using a template literal to display some info 
         // about each game
         // TIP: if your images are not displaying, make sure there is space
         // between the end of the src attribute and the end of the tag ("/>")
-
+        gameCard.innerHTML = `
+            <h3>${game.name}</h3>
+            <p>${game.description}</p>
+            <p>Backers: ${game.backers}</p>
+            <img class="game-img" src="${game.img}" />
+        `;
 
         // append the game to the games-container
-
+        gamesContainer.appendChild(gameCard);
+    }
 }
 
 // call the function we just defined using the correct variable
 // later, we'll call this function using a different list of games
-
+addGamesToPage(GAMES_JSON);
 
 /*************************************************************************************
  * Challenge 4: Create the summary statistics at the top of the page displaying the
@@ -61,19 +68,24 @@ function addGamesToPage(games) {
 const contributionsCard = document.getElementById("num-contributions");
 
 // use reduce() to count the number of total contributions by summing the backers
-
+const totalContributions = GAMES_JSON.reduce((sum, game) => sum + game.backers, 0);
 
 // set the inner HTML using a template literal and toLocaleString to get a number with commas
+contributionsCard.innerHTML = `${totalContributions.toLocaleString()}`;
 
+// grab the amount raised card element
+const amountRaisedCard = document.getElementById("total-raised");
 
-// grab the amount raised card, then use reduce() to find the total amount raised
-const raisedCard = document.getElementById("total-raised");
+//use reduce() to find the total amount raised by summing the pledged amounts
+const totalRaised = GAMES_JSON.reduce((acc, game) => acc + game.pledged, 0);
 
 // set inner HTML using template literal
+amountRaisedCard.innerHTML = `$${totalRaised.toLocaleString()}`;
 
-
-// grab number of games card and set its inner HTML
+// grab number of games card and set its inner HTML (does not need reduce() method)
 const gamesCard = document.getElementById("num-games");
+gamesCard.innerHTML = `${GAMES_JSON.length}`;
+
 
 
 /*************************************************************************************
@@ -87,10 +99,12 @@ function filterUnfundedOnly() {
     deleteChildElements(gamesContainer);
 
     // use filter() to get a list of games that have not yet met their goal
-
+    let listOfUnfundedGames = GAMES_JSON.filter((game) => {
+        return game.pledged < game.goal;
+    })
 
     // use the function we previously created to add the unfunded games to the DOM
-
+    addGamesToPage(listOfUnfundedGames);
 }
 
 // show only games that are fully funded
@@ -98,10 +112,12 @@ function filterFundedOnly() {
     deleteChildElements(gamesContainer);
 
     // use filter() to get a list of games that have met or exceeded their goal
-
+    let listOfFundedGames = GAMES_JSON.filter((game) => {
+        return game.pledged >= game.goal;
+    })
 
     // use the function we previously created to add unfunded games to the DOM
-
+    addGamesToPage(listOfFundedGames);
 }
 
 // show all games
@@ -109,7 +125,7 @@ function showAllGames() {
     deleteChildElements(gamesContainer);
 
     // add all games from the JSON data to the DOM
-
+    addGamesToPage(GAMES_JSON);
 }
 
 // select each button in the "Our Games" section
@@ -118,6 +134,10 @@ const fundedBtn = document.getElementById("funded-btn");
 const allBtn = document.getElementById("all-btn");
 
 // add event listeners with the correct functions to each button
+unfundedBtn.addEventListener("click", filterUnfundedOnly);
+fundedBtn.addEventListener("click", filterFundedOnly);
+allBtn.addEventListener("click", showAllGames);
+
 
 
 /*************************************************************************************
@@ -129,12 +149,19 @@ const allBtn = document.getElementById("all-btn");
 const descriptionContainer = document.getElementById("description-container");
 
 // use filter or reduce to count the number of unfunded games
+let listOfUnfundedGames = GAMES_JSON.filter((game) => {
+    return game.pledged < game.goal;
+})
+const numOfUnfunded = listOfUnfundedGames.length;
 
 
 // create a string that explains the number of unfunded games using the ternary operator
-
+const displayStr = `A total of $${totalRaised.toLocaleString()} has been raised for ${GAMES_JSON.length} ${GAMES_JSON.length == 1 ? "game" : "games"}. Currently, ${numOfUnfunded} ${numOfUnfunded == 1 ? "game remains" : "games remain"} unfunded. We need your help to fund these amazing games!`;
 
 // create a new DOM element containing the template string and append it to the description container
+const unfundedDescElement = document.createElement("p");
+unfundedDescElement.innerText = displayStr;
+descriptionContainer.appendChild(unfundedDescElement);
 
 /************************************************************************************
  * Challenge 7: Select & display the top 2 games
@@ -149,7 +176,26 @@ const sortedGames =  GAMES_JSON.sort( (item1, item2) => {
 });
 
 // use destructuring and the spread operator to grab the first and second games
+const [firstGame, secondGame, ...otherGames] = sortedGames;
 
 // create a new element to hold the name of the top pledge game, then append it to the correct element
+const firstGameElement = document.createElement("p");
+firstGameElement.innerText = firstGame.name;
+firstGameContainer.appendChild(firstGameElement);
 
 // do the same for the runner up item
+const secondGameElement = document.createElement("p");
+secondGameElement.innerText = secondGame.name;
+secondGameContainer.appendChild(secondGameElement);
+
+/************************************************************************************
+ * Custom Formatting Done Below
+ */
+
+const targetSection = document.getElementById("our-games-header");
+const scrollButton = document.getElementById("scroll-button");
+
+scrollButton.addEventListener("click", () => {
+    targetSection.scrollIntoView();
+});
+
